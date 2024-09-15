d3486d6a69a4ce434f1dd61fb3751374b6c43236
gonyango <grantononyango@gmail.com>
2024-09-15 11:55:37 +0300

    add poll and job stories

--- api.js
+++ api.js
@@ -1,11 +1,17 @@
-const API_BASE_URL = 'https://hacker-news.firebaseio.com/v0/';
+const API_BASE_URL = "https://hacker-news.firebaseio.com/v0/";
 
-export async function fetchLatestItemIds() {
-    const response = await fetch(`${API_BASE_URL}newstories.json`);
-    return await response.json();
+export async function fetchLatestItemIds(type = "stories") {
+  const endpoint =
+    type === "jobs"
+      ? "jobstories"
+      : type === "polls"
+      ? "pollstories"
+      : "newstories";
+  const response = await fetch(`${API_BASE_URL}${endpoint}.json`);
+  return await response.json();
 }
 
 export async function fetchItem(id) {
-    const response = await fetch(`${API_BASE_URL}item/${id}.json`);
-    return await response.json();
+  const response = await fetch(`${API_BASE_URL}item/${id}.json`);
+  return await response.json();
 }
